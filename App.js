9516a78ea3c29b759dbab23121c5fbb2e42dbe43
ferdythebull <prestonbernstein@gmail.com>
2015-03-18 14:10:09 -0400

    MVP of product

--- App.js
+++ App.js
@@ -1,30 +1,38 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.TimeboxScopedApp',
+    layout:{
+    	type: 'vbox',
+    	align: 'stretch'
+    },
     componentCls: 'app',
     scopeType: 'release',
     comboboxConfig:{
     	fieldLabel: 'Select a Release:',
     	labelWidth: 100,
-    	width: 300
+    	width: 300,
+    	itemId:'release-combobox',
+    	autoRender: true
     },
-
+    
     scheduledStore: undefined,
     scheduledGrid: undefined,
     emailButton: undefined,
 
-    _getFilters: function(){
-        var releaseFilter = this.getContext().getTimeboxScope().getQueryFilter();
-
-        this.onScopeChange(releaseFilter);
-    },
-
-    onScopeChange: function(releaseFilter) {
+    onScopeChange: function() {
         var releaseFilter = this.getContext().getTimeboxScope().getQueryFilter();
 
         if(!this.scheduledStore){
         	this.scheduledStore = Ext.create('Rally.data.wsapi.artifact.Store',{
         		models: ['UserStory','Defect'],
         		autoLoad: true,
+                storeConfig:{
+                    context:{
+                        project: "/project/4913344213",
+                        projectScopeDown:"true",
+                        projectScopeUp:"true",
+                        workspace: "/workspace/615803530"
+                    }
+                },
         		filters:releaseFilter,
         		listeners: {
         			load: this._onDataLoaded,
@@ -73,11 +81,14 @@ Ext.define('CustomApp', {
             enableEditing: false
         });
         this.add(this.scheduledGrid);
+
+        console.log(this.getContext().getDataContext());
     },
 
     _createEmailButton: function(myRecords){
         this.emailButton = Ext.create('Ext.Button',{
-            text: 'Send Email',
+            text: 'Write Email',
+            xtype: 'email-button',
             listeners: {
             	load: this._onDataLoaded,
             	scope: this
@@ -86,20 +97,72 @@ Ext.define('CustomApp', {
        	        if(myRecords!=null){
     				console.log(myRecords);
     			};
-            	alert('The email has been sent!');
+                _createWindow
+            	var myForm = Ext.create('Ext.window.Window',{
+            		title: 'Write Email',
+            		height: 250,
+            		width: 400,
+            		items:[{
+            			xtype: 'fieldcontainer',
+            			labelWidth: 100,
+            			layout: 'anchor',
+            			defaults: {
+            				layout: 'fit'
+            			},
 
-            	Ext.Ajax.request({
-            		url: 'http://localhost:8080/MailApi/mails/sendMail',
-            		method: 'POST',
-            		params: {
-            			requestParam: 'notInRequestBody'
-            		},
-            		success: function(){console.log('success');},
-            		failure: function(){Ext.Msg.alert('Failure to send message');},
-            		jsonData:myRecords
-            	});
+            			fieldDefaults: {
+            				msgTarget: 'under',
+            				labelAlign: 'top'
+            			},
+
+            			items: [{
+                            xtype: 'textfield',
+            				fieldLabel: '<strong><em>Required</em></strong><br> Subject',
+            				name: 'subject',
+                            width: 300,
+                            id: 'subject',
+                            blankText: 'This field is required',
+                            emptyText: "Write here..."
+            			}, {
+                            xtype: 'textareafield',
+            				fieldLabel: '<strong><em>Optional</em></strong><br> Introductory Paragraph',
+            				name: 'introductoryParagraph',
+                            height: 100,
+                            width: 300,
+                            id: 'introductoryParagraph',
+            				msgTarget: 'under',
+                            emptyText: 'Introduce the release notes here...'
+            			}]
+            		}],
+            		buttons: [{
+            			text: 'Submit',
+            			handler: function(){
+                            var vals = {
+                                'Subject' : Ext.getCmp('subject').getValue(),
+                                'IntroductoryParagraph' : Ext.getCmp('introductoryParagraph').getValue()
+                            };
+                            myRecords.push(vals);
+                            myRecords.filter(function(n){return n === 0 || n});
+
+            				myForm.close();
+
+            				Ext.Ajax.request({
+    		            		// url: 'https://HERMES/MailApi/mails/sendMail',
+    		            		method: 'POST',
+    		            		params: {
+    		            			requestParam: 'notInRequestBody'
+    		            		},
+    		            		success: function(){Ext.Msg.alert('success');},
+    		            		failure: function(){Ext.Msg.alert('Failure to send message');},
+    		            		jsonData:myRecords
+
+		            	 });
+
+                            myRecords = [];
+            			}
+            		}]
+            	}).show();
             }
         });
     }
-
 });
